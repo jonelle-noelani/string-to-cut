6daad3a287d8a1ab5107bf074a873f04f190786e
jonelle-noelani <jonelle.noelani@gmail.com>
2021-06-21 16:45:41 -0700

    added cutString function

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-// const BASE_URL = "http://localhost:3000"
-const BASE_URL = "https://lyft-interview-test.glitch.me/test" 
+const BASE_URL = "http://localhost:3000"
+// const BASE_URL = "https://lyft-interview-test.glitch.me/test" 
 
-cutString('{"string_to_cut":"iamyourlyftdriver"}')
+postString('{"string_to_cut":"iamyourlyftdriver"}')
 
-function cutString(string_to_cut){
+function postString(string_to_cut){
     fetch(BASE_URL, {
         method: 'POST',
         headers: {
             'Content-Type':'application/json',
             'Accept':'application/json'
-            // 'Access-Control-Allow-Origin':'no-cors',
-            // 'Access-Control-Allow-Methods': 'POST'
         },
         body: JSON.stringify(string_to_cut)
     })
     .then(res => res.json())
-    .then(string_to_cut => {
-        console.log(string_to_cut)
+    .then(str => {
+        cutString(str)
     })
+}
+
+// cutString("iamyourlyftdriver")
+
+function cutString(str){
+    let everyThird = ''
+    const splitStr = str.split('')
+    for (let i = 2; i < splitStr.length; i += 3){
+        everyThird = everyThird.concat(splitStr[i])
+    }
+    console.log({"return_string": everyThird})
 }
